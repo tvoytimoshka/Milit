a2fae8f8670a058d7c5f579ae75ede58f684508d
Timofey <temosmitenev@gmail.com>
2022-12-14 12:40:24 +0300

    settings item and other

---
+++ app/src/main/java/com/example/neighbour/ui/general/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.neighbour.ui.general;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.neighbour.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
