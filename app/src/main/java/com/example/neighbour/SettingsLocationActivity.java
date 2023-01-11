8c22b0855f8cbaa446ceab774d75f1753a2ecaf1
Timofey <temosmitenev@gmail.com>
2023-01-11 15:24:41 +0300

    add new settings

---
+++ app/src/main/java/com/example/neighbour/SettingsLocationActivity.java
@@ -0,0 +1,14 @@
+package com.example.neighbour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_location);
+    }
+}
